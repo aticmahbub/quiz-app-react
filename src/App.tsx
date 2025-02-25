dbc59b8bfe28c669cf22e12e3f4e92cd1d14a4d6
aticcmahbub <aticcmahbub@gmail.com>
2025-02-26 01:28:24 +0600

    feat: integrate api to fetch questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import {useState} from 'react';
 import QuestionCard from './components/QuestionCard';
+import {Difficulty, fetchQuestions} from './API';
 
 const TOTAL_QUESTIONS = 10;
 function App() {
     const [loading, setLoading] = useState(false);
     const [questions, setQuestions] = useState([]);
     const [number, setNumber] = useState(0);
-    const [userAnswers, setUserAnswers] = [];
-    const [score, setScore] = 0;
-    const [gameOver, setGameOver] = true;
+    const [userAnswers, setUserAnswers] = useState([]);
+    const [score, setScore] = useState(0);
+    const [gameOver, setGameOver] = useState(true);
+
+    console.log(fetchQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
 
     const startTrivia = () => {};
 
@@ -24,14 +27,14 @@ function App() {
             </button>
             <p className='score'>Score</p>
             <p>Loading...</p>
-            <QuestionCard
+            {/* <QuestionCard
                 questionNumber={number + 1}
                 totalQuestions={TOTAL_QUESTIONS}
                 question={questions[number].question}
                 answers={questions[number].answers}
                 userAnswer={userAnswers ? userAnswers[number] : undefined}
                 callback={checkAnswer}
-            />
+            /> */}
             <button className='next' onClick={nextQuestion}>
                 Next Question
             </button>
