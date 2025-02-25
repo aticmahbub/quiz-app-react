3cdf8e6e6a0c109ae3d6fc1fe69ce3294829cd90
aticcmahbub <aticcmahbub@gmail.com>
2025-02-26 00:19:30 +0600

    create states

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import {useState} from 'react';
+import QuestionCard from './components/QuestionCard';
+
+const TOTAL_QUESTIONS = 10;
 function App() {
+    const [loading, setLoading] = useState(false);
+    const [questions, setQuestions] = useState([]);
+    const [number, setNumber] = useState(0);
+    const [userAnswers, setUserAnswers] = [];
+    const [score, setScore] = 0;
+    const [gameOver, setGameOver] = true;
+
+    const startTrivia = () => {};
+
+    const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+    const nextQuestion = () => {};
+
     return (
         <div className='App'>
-            <h2>Quiz</h2>
+            <h2>React Quiz App</h2>
+            <button className='start' onClick={startTrivia}>
+                Start
+            </button>
+            <p className='score'>Score</p>
+            <p>Loading...</p>
+            <QuestionCard
+                questionNumber={number + 1}
+                totalQuestions={TOTAL_QUESTIONS}
+                question={questions[number].question}
+                answers={questions[number].answers}
+                userAnswer={userAnswers ? userAnswers[number] : undefined}
+                callback={checkAnswer}
+            />
+            <button className='next' onClick={nextQuestion}>
+                Next Question
+            </button>
         </div>
     );
 }
